0b3ce261f26336c7d7979572d01331eb99eee2a1
jgolden6 <jgolden6@my.westga.edu>
2022-03-04 02:52:10 -0500

    Added a test project with tests.
    I created a new test project and added tests for all the shared library model classes. I also created a coverage report and class diagram.

---
+++ code/TheTripMasterTest/LibraryModel/WaypointTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheTripMasterLibrary.Model;
+
+namespace TheTripMasterTest.LibraryModel
+{
+    [TestClass]
+    public class WaypointTest
+    {
+        [TestMethod]
+        public void TestWaypoint()
+        {
+            Waypoint waypoint = new Waypoint
+            {
+                WaypointId = 1,
+                TripId = 1,
+                WaypointName = "Waypoint1",
+                TripName = "Trip1",
+                StartDate = DateTime.MaxValue,
+                EndDate = DateTime.MaxValue,
+            };
+
+            waypoint.WaypointId = 2;
+            waypoint.TripId = 2;
+            waypoint.WaypointName = "Waypoint2";
+            waypoint.TripName = "Trip2";
+            waypoint.StartDate = DateTime.MinValue;
+            waypoint.EndDate = DateTime.MinValue;
+
+            Assert.AreEqual(2, waypoint.WaypointId);
+            Assert.AreEqual(2, waypoint.TripId);
+            Assert.AreEqual("Waypoint2", waypoint.WaypointName);
+            Assert.AreEqual("Trip2", waypoint.TripName);
+            Assert.AreEqual(DateTime.MinValue, waypoint.StartDate);
+            Assert.AreEqual(DateTime.MinValue, waypoint.EndDate);
+        }
+    }
+}
