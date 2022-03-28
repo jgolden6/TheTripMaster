e184d2cd6083ed7cea2a168bc2ee9fa8e08d35af
Jacob Pefinis <JPefinis@AcrossHealthcare.com>
2022-03-28 06:50:34 -0400

    Finished testing DataLayer classes.
    Fully tested the data layer classes in the shared library.

---
+++ code/TheTripMasterTest/LibraryDataLayer/LodgingDataLayerTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheTripMasterLibrary.DataLayer;
+using TheTripMasterLibrary.Model;
+
+namespace TheTripMasterTest.LibraryDataLayer
+{
+    [TestClass]
+    public class LodgingDataLayerTest
+    {
+        [TestMethod]
+        public void TestAddLodging()
+        {
+            LodgingDataLayer dataLayer = new LodgingDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Lodging newLodging = new Lodging
+            {
+                StreetAddress = "80 Street St",
+                City = "City",
+                State = "GA",
+                ZipCode = "30110",
+                Description = "",
+                TripName = "Belgium",
+                StartDate = DateTime.Parse("7/2/2022 12:00:00 AM"),
+                EndDate = DateTime.Parse("7/3/2022 12:00:00 AM")
+            };
+
+            SelectedTrip.Trip = new Trip { TripId = 18 };
+            dataLayer.AddLodging(newLodging);
+            Lodging lodging = dataLayer.GetTripLodgings(18)[0];
+            dataLayer.DeleteLodging(lodging.LodgingId);
+
+            Assert.AreEqual("80 Street St", lodging.StreetAddress.Trim());
+            Assert.AreEqual("City", lodging.City.Trim());
+            Assert.AreEqual("GA", lodging.State.Trim());
+            Assert.AreEqual("30110", lodging.ZipCode.Trim());
+            Assert.AreEqual(DateTime.Parse("7/2/2022 12:00:00 AM"), lodging.StartDate);
+            Assert.AreEqual(DateTime.Parse("7/3/2022 12:00:00 AM"), lodging.EndDate);
+        }
+
+        [TestMethod]
+        public void TestGetTripLodgings()
+        {
+            LodgingDataLayer dataLayer = new LodgingDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            List<Lodging> lodgings = dataLayer.GetTripLodgings(25);
+
+            Assert.AreEqual(2, lodgings.Count);
+            Assert.AreEqual("60 Street St", lodgings[0].StreetAddress.Trim());
+            Assert.AreEqual("70 Road Rd", lodgings[1].StreetAddress.Trim());
+        }
+
+        [TestMethod]
+        public void TestGetLodging()
+        {
+            LodgingDataLayer dataLayer = new LodgingDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Lodging lodging = dataLayer.GetLodging(2);
+
+            Assert.AreEqual("60 Street St", lodging.StreetAddress.Trim());
+        }
+
+        [TestMethod]
+        public void TestDeleteLodging()
+        {
+            LodgingDataLayer dataLayer = new LodgingDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Lodging newLodging = new Lodging
+            {
+                StreetAddress = "90 Road St",
+                City = "City",
+                State = "GA",
+                ZipCode = "30110",
+                Description = "",
+                TripName = "Belgium",
+                StartDate = DateTime.Parse("7/5/2022 12:00:00 AM"),
+                EndDate = DateTime.Parse("7/6/2022 12:00:00 AM")
+            };
+
+            SelectedTrip.Trip = new Trip { TripId = 6 };
+            dataLayer.AddLodging(newLodging);
+            Lodging lodging = dataLayer.GetTripLodgings(6)[0];
+            dataLayer.DeleteLodging(lodging.LodgingId);
+            List<Lodging> transports = dataLayer.GetTripLodgings(6);
+
+            Assert.AreEqual(0, transports.Count);
+        }
+    }
+}
---
+++ code/TheTripMasterTest/LibraryDataLayer/TripDataLayerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheTripMasterLibrary.DataLayer;
+using TheTripMasterLibrary.Model;
+
+namespace TheTripMasterTest.LibraryDataLayer
+{
+    [TestClass]
+    public class TripDataLayerTest
+    {
+        [TestMethod]
+        public void TestAddTrip()
+        {
+            ActiveUser.User = new User {UserId = 1};
+            TripDataLayer dataLayer = new TripDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Trip newTrip = new Trip
+            {
+                Name = "Vacation",
+                StartDate = DateTime.Now.Add(new TimeSpan(1, 0, 0, 0)),
+                EndDate = DateTime.Now.Add(new TimeSpan(3, 0, 0, 0))
+            };
+
+            dataLayer.AddTrip(newTrip);
+            Trip trip = dataLayer.GetSelectedTrip("Vacation");
+            this.RemoveTestTrip();
+
+            Assert.IsNotNull(trip.Name);
+        }
+
+        [TestMethod]
+        public void TestGetAllTrips()
+        {
+            TripDataLayer dataLayer = new TripDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            List<Trip> trips = dataLayer.GetAllTripsOfUser(1005);
+
+            Assert.AreEqual(4, trips.Count);
+            Assert.IsNotNull(trips[0].Name);
+        }
+
+        [TestMethod]
+        public void TestUpdateTrip()
+        {
+            ActiveUser.User = new User { UserId = 1005 };
+            TripDataLayer dataLayer = new TripDataLayer();
+            dataLayer.SetConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            dataLayer.UpdateTrip("Paris", DateTime.Now.Add(new TimeSpan(500, 0, 0, 0)), DateTime.Now.Add(new TimeSpan(501, 0, 0, 0)));
+            Trip trip = dataLayer.GetSelectedTrip("Paris");
+            dataLayer.UpdateTrip("Paris", DateTime.Parse("10/1/2022 12:00:00 AM"), DateTime.Parse("11/1/2022 12:00:00 AM"));
+
+            Assert.AreNotEqual(DateTime.Parse("5/1/2022 12:00:00 AM"), trip.StartDate);
+        }
+
+        private void RemoveTestTrip()
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM [Trip] WHERE userId = @userId AND tripName = @tripName";
+
+                cmd.Parameters.AddWithValue("@userId", 1);
+                cmd.Parameters.AddWithValue("@tripName", "Vacation");
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+    }
+}
